3717dd8b77a70978f46fadbb9cbd28bd93856435
Gowtham Potnuru <gouthamcall@gmail.com>
2022-11-22 15:40:41 -0500

    updated

--- geolocation.py
+++ geolocation.py
@@ -1,16 +1,30 @@
 import re
 import json
 from urllib.request import urlopen
-from math import radians, cos, sin, asin, sqrt
+from math import radians, cos, sin, asin, sqrt, inf
 
-def distance(lat1, lon1, lat2, lon2):
+def find_closest_replica_server(source_ip):
+    source_coordinates = find_location_coordinates(source_ip)
+    replica_servers = ['175.253.115.99', '142.250.72.100', '204.44.192.60']
+    min_dist = inf
+    nearest_server = replica_servers[0]
+
+    for server in replica_servers:
+        server_coordinates = find_location_coordinates(server)
+        dist = distance_between_locations(server_coordinates, source_coordinates)
+        if dist < min_dist:
+            nearest_server = server
+
+    return nearest_server
+
+def distance_between_locations(loc1, loc2):
      
     # The math module contains a function named
     # radians which converts from degrees to radians.
-    lon1 = radians(lon1)
-    lon2 = radians(lon2)
-    lat1 = radians(lat1)
-    lat2 = radians(lat2)
+    lon1 = radians(loc1[0])
+    lon2 = radians(loc1[1])
+    lat1 = radians(loc2[0])
+    lat2 = radians(loc2[1])
       
     # Haversine formula
     dlon = lon2 - lon1
@@ -29,12 +43,11 @@ def find_location_coordinates(ip):
     url = 'http://ipinfo.io/' + ip + '/json'
     response = urlopen(url)
     data = json.load(response)
+    print(data)
     lat = float(data['loc'].split(",")[0])
     lon = float(data['loc'].split(",")[1])
     
-    return lat, lon
+    return (lat, lon)
 
-lat2 = 63
-lon2 = -5.79
-
-print(distance(*find_location_coordinates('175.253.115.99'), lat2, lon2))
+find_location_coordinates('172.24.48.1')
+# print(find_closest_replica_server('172.24.48.1'))
